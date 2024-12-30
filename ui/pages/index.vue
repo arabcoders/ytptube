0359eafbec93802afbd963ec8830f2e4bc9f191b
ArabCoders <admin@arabcoders.org>
2024-12-30 11:03:01 +0300

    fix updating max_workers in UI

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -8,28 +8,27 @@
 <script setup>
 const config = useConfigStore()
 const stateStore = useStateStore()
-const maxWorkers = config.app.max_workers
 
 onMounted(() => {
   if (!config.app.ui_update_title) {
     useHead({ title: 'YTPTube' })
     return
   }
-  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${maxWorkers} | ${Object.keys(stateStore.history).length || 0} )` })
+  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${config.app.max_workers} | ${Object.keys(stateStore.history).length || 0} )` })
 })
 
 watch(() => stateStore.history, () => {
   if (!config.app.ui_update_title) {
     return
   }
-  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${maxWorkers}  | ${Object.keys(stateStore.history).length || 0} )` })
+  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${config.app.max_workers}  | ${Object.keys(stateStore.history).length || 0} )` })
 }, { deep: true })
 
 watch(() => stateStore.queue, () => {
   if (!config.app.ui_update_title) {
     return
   }
-  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${maxWorkers}  | ${Object.keys(stateStore.history).length || 0} )` })
+  useHead({ title: `YTPTube: ( ${Object.keys(stateStore.queue).length || 0}/${config.app.max_workers}  | ${Object.keys(stateStore.history).length || 0} )` })
 }, { deep: true })
 
 </script>
