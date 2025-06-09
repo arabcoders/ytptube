e251fba26c5828720a5e4aeae86386692cad3d56
arabcoders <admin@arabcoders.org>
2025-06-09 20:18:45 +0300

    Fix healthcheck when used with base_path

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,6 +2,7 @@
 set -euuo pipefail
 
 LOCAL_PORT="${YTP_PORT:-8081}"
+BASE_PATH="${YTP_BASE_PATH:-/}"
 
 curl_args=(-f)
 
@@ -10,5 +11,8 @@ if [[ -n "${YTP_AUTH_USERNAME:-}" && -n "${YTP_AUTH_PASSWORD:-}" ]]; then
   curl_args+=(-H "Authorization: Basic ${cred}")
 fi
 
-curl_args+=("http://localhost:${LOCAL_PORT}/api/ping")
+# strip trailing slashes from BASE_PATH
+BASE_PATH=$(echo "${BASE_PATH}" | sed 's:/*$::')
+
+curl_args+=("http://localhost:${LOCAL_PORT}${BASE_PATH}/api/ping")
 /usr/bin/curl "${curl_args[@]}"
